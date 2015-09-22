3d3bfd8b9f435f9c9475d661d7639ccea5f0c31d
Wei-ju Wu <weiju.wu@gmail.com>
2015-09-22 14:25:17 -0700

    saving of user setting changes
     - user can update the system
     - saves settings

--- config_server.c
+++ config_server.c
@@ -15,10 +15,11 @@
 
 #define HTDOCS_PREFIX "htdocs"
 /* Length of the opening and closing option tags <option "..."></option> */
-#define OPTION_TAG_LENGTH (11 + 9)
+#define OPTION_TAG_LENGTH (17 + 9)
 #define REFRESH_TOKEN_PREFIX "refresh_token="
 #define SYSTEM_UID_PREFIX    "system_uid="
 #define SERVICE_PORT_PREFIX  "service_port="
+#define DEFAULT_SERVICE_PORT 8080
 
 #define INI_PATH "config.ini"
 
@@ -28,6 +29,26 @@ struct token_post_data {
   struct MHD_PostProcessor *pp;
   char token[OAUTH2_TOKEN_MAXLEN + 1];
 };
+
+struct system_post_data {
+  struct MHD_PostProcessor *pp;
+  char system_uid[SYSTEM_UID_MAXLEN + 1];
+};
+
+/* Store updated configuration */
+static int save_configuration()
+{
+  FILE *fp = fopen("new.ini", "w");
+  if (fp) {
+    fprintf(fp, "refresh_token=%s\nsystem_uid=%s\nservice_port=%d\n",
+            client_config.oauth2_refresh_token,
+            client_config.system_uid,
+            client_config.service_port);
+    fclose(fp);
+    return 1;
+  }
+  return 0;
+}
 
 /*
  * Handle the HTTP server requests here.
@@ -41,6 +62,9 @@ static int handle_get(struct MHD_Connection *connection, const char *url)
   int ret = 0, is_static = 1, has_token = 0;
   const char *filepath;
   char *path_buffer = NULL;
+
+  LOG_DEBUG("token: %s\n", client_config.oauth2_refresh_token);
+  has_token = strlen(client_config.oauth2_refresh_token);
 
   /* Check and process routes */
   if (!strcmp(url, "/")) {
@@ -105,12 +129,16 @@ static int handle_get(struct MHD_Connection *connection, const char *url)
 
       option_buffer = calloc(buffersize + 2, sizeof(char));
       for (i = 0; i < entries->num_entries; i++) {
-        strcat(option_buffer, "<option \"");
+        strcat(option_buffer, "<option value=\"");
         strcat(option_buffer, entries->entries[i].uid);
         strcat(option_buffer, "\">");
         strcat(option_buffer, entries->entries[i].name);
         strcat(option_buffer, "</option>");
       }
+      /*
+      if (strlen(client_options.system_uid)) {
+      }*/
+      stemp_dict_put(dict, "current_system", "none");
       stemp_dict_put(dict, "system_options", option_buffer);
       aqx_free_systems(entries);
       free(option_buffer);
@@ -151,6 +179,20 @@ static int iterate_post(void *coninfo_cls, enum MHD_ValueKind kind, const char *
   return MHD_YES;
 }
 
+static int iterate_system_post(void *coninfo_cls, enum MHD_ValueKind kind, const char *key,
+                               const char *filename, const char *content_type,
+                               const char *transfer_encoding, const char *data, 
+                               uint64_t off, size_t size)
+{
+  struct system_post_data *post_data = (struct system_post_data *) coninfo_cls;
+  if (post_data && !strcmp("system", key)) {
+    strncpy(post_data->system_uid, data, SYSTEM_UID_MAXLEN);
+    return MHD_NO;
+  }
+  return MHD_YES;
+}
+
+
 /*
   The simplest way to implement the post processor handling.
   We create the post processor at the beginning and
@@ -163,9 +205,9 @@ static int handle_post(struct MHD_Connection *connection,
 {
   struct MHD_Response *response;
   /* user data is currently post processor */
-  struct token_post_data *post_data = *con_cls;
   int ret = 0;
   if (!strcmp("/enter-token", url)) {
+    struct token_post_data *post_data = (struct token_post_data *) *con_cls;
     if (!post_data) {
       post_data = (struct token_post_data *) calloc(1, sizeof(struct token_post_data));
       if (post_data) {
@@ -187,8 +229,12 @@ static int handle_post(struct MHD_Connection *connection,
         LOG_DEBUG("ACCESS TOKEN obtained: %s\n", post_data->token);
         refresh_token = aqx_get_refresh_token(post_data->token);
         if (refresh_token) {
-          LOG_DEBUG("refresh token: '%s'\n", refresh_token);
-          /* copy the valid token into the configuration */
+          /* copy the valid token into the configuration and save */
+          LOG_DEBUG("new refresh token: '%s'\n", refresh_token);
+          strncpy(client_config.oauth2_refresh_token, refresh_token, OAUTH2_TOKEN_MAXLEN);
+          if (save_configuration()) {
+            aqx_client_update_refresh_token(client_config.oauth2_refresh_token);
+          }
         }
 
         MHD_destroy_post_processor(post_data->pp);
@@ -201,6 +247,41 @@ static int handle_post(struct MHD_Connection *connection,
         return ret;
       }
     }
+  } else if (!strcmp("/set-system", url)) {
+    struct system_post_data *post_data = (struct system_post_data *) *con_cls;
+    if (!post_data) {
+      post_data = (struct system_post_data *) calloc(1, sizeof(struct system_post_data));
+      if (post_data) {
+        post_data->pp = MHD_create_post_processor(connection, 1024, iterate_system_post,
+                                                  post_data);
+        *con_cls = (void *) post_data;
+      }
+      return MHD_YES;
+    } else {
+      if (*upload_data_size) {
+        MHD_post_process(post_data->pp, upload_data, *upload_data_size);
+        /* setting upload data to 0 says "processed" */
+        *upload_data_size = 0;
+        return MHD_YES;
+      } else {
+        /* No more data */
+        /* We implement the PRG pattern here (POST-Redirect-GET) */
+        LOG_DEBUG("System UID obtained: %s\n", post_data->system_uid);
+        strncpy(client_config.system_uid, post_data->system_uid, SYSTEM_UID_MAXLEN);
+        if (save_configuration()) {
+          aqx_client_update_system(client_config.system_uid);
+        }
+        MHD_destroy_post_processor(post_data->pp);
+        free(post_data);
+
+        response = MHD_create_response_from_buffer(2, "ok", MHD_RESPMEM_PERSISTENT);      
+        MHD_add_response_header(response, "Location", "/");
+        ret = MHD_queue_response(connection, MHD_HTTP_FOUND, response);
+        MHD_destroy_response(response);
+        return ret;
+      }
+    }
+    
   } else {
     response = MHD_create_response_from_buffer(5, "error", MHD_RESPMEM_PERSISTENT);      
     ret = MHD_queue_response(connection, MHD_HTTP_FOUND, response);
@@ -250,6 +331,10 @@ static void parse_config_line(struct aqx_client_options *cfg, char *line)
  * Public API
  **********************************************************************/
 
+/*
+ * This is the entry point of the API client library, reading the
+ * configuration.
+ */
 struct aqx_client_options *aqx_client_init()
 {
   FILE *fp = fopen(INI_PATH, "r");
@@ -263,10 +348,11 @@ struct aqx_client_options *aqx_client_init()
     LOG_DEBUG("system_uid: '%s', refresh_token: '%s'\n",
               client_config.system_uid, client_config.oauth2_refresh_token);
     fclose(fp);
+  } else {
+    client_config.service_port = DEFAULT_SERVICE_PORT;
   }
   aqx_client_update_refresh_token(client_config.oauth2_refresh_token);
   aqx_client_update_system(client_config.system_uid);
-
   aqx_client_init_api();
   return &client_config;
 }
